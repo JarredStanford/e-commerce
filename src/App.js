9889566a7ae01befafbfff28d77cd279de794820
JarredStanford <stanj89@gmail.com>
2020-02-20 01:59:52 -0500

    starting auth0

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 import { Grommet } from 'grommet'
 
 import Dashboard from './components/Dashboard'
 
+import { useAuth0 } from "./components/react-auth0-wrapper";
+
 function App() {
+
+  const { loading, user, isAuthenticated } = useAuth0();
+
+  const call = async () => {
+    const games = await axios.get('https://baseball-league.herokuapp.com/data/player/mlb.p.8861/')
+    console.log(games)
+  }
+
 
 
   return (
     <Grommet plain>
       <Dashboard />
+      <button onClick={() => call()}>nah</button>
     </Grommet>
   )
 
